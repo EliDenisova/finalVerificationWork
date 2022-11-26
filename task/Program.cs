b664fe8610ff874267652e7011f8deafb9b89205
Elena <pyatochkaa@mail.ru>
2022-11-27 00:41:37 +0300

    fixed

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = 0;
+String[] array = new String[5];
+String[] newArray = new String[array.Length];
+Console.WriteLine("enter the words");
+
+for (int i = 0; i < array.Length; i++)
+{
+    String word = Console.ReadLine();
+    array[i] = word;
+}
+
+Console.WriteLine(" ");
+Console.WriteLine("the original array");
+arrayOutput(array);
+formingNewArray(array, newArray);
+Console.WriteLine("newly formed array");
+arrayOutput(newArray);
+
+
+void formingNewArray (String[] array, String[] newArray){
+int j = 0;
+for (int i = 0; i < newArray.Length; i++)
+{
+    if(array[i].Length <= 3) {
+        newArray[j++] = array[i];
+    }
+}
+}
+
+
+void arrayOutput(String[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} "); 
+    }
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
+}
